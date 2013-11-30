db418e9fd1298ad1386ca8e02fc0bc49bc9359ff
Vishal Chandnani <vschandnani@gmail.com>
2013-11-29 23:53:43 -0500

    [VC] "search and replace" strings program
    - added functions to get archive, query and replacement strings
    - modified help to reflect archive, query and replacement strings

--- sar.c
+++ sar.c
@@ -3,35 +3,85 @@
 void display_help()
 {
   system("tput clear");
-  puts("\n Welcome to the Search and Replace Program ");
-  puts(" This program searches a search string for a query string. ");
-  puts(" If the search string contains the entire query string, ");
-  puts(" it replaces the query string with the newer string. ");
-  puts(" Example:");
-  puts(" Search String = \"hello world\" ");
-  puts(" Query String = \"world\" ");
-  puts(" Newer String  = \"people\" ");
-  puts(" Result String = \"hello people\" ");
+  puts("  Welcome to the Search and Replace Program.");
+  puts("  Inputs:  Archive String, Query String and Replacement String.");
+  puts("  Outputs: Result String.");
+  puts("  The Search and Replace Program searches the archive string for a query string.");
+  puts("  If the archive string contains the entire query string, it replaces the query");
+  puts("  string with the replacement string and displays the final result string.");
+  puts("  Example:");
+  puts("  Archive String:     \"hello world\"");
+  puts("  Query String:       \"world\"");
+  puts("  Replacement String: \"people\"");
+  puts("  Result String:      \"hello people\"");
+}
+
+char str1[256], str2[256], str3[256];
+
+char *get_archive_string()
+{
+  puts("  Enter the archive string: ");
+  scanf("%s", str1);
+  return str1;
+}
+
+char *get_query_string()
+{
+  puts("  Enter the query string: ");
+  scanf("%s", str2);
+  return str2;
+}
+
+char *get_replacement_string()
+{
+  puts("  Enter the replacement string: ");
+  scanf("%s", str3);
+  return str3;
 }
 
 main()
 {
+  char *ptr1, *ptr2, *ptr3;
+//  bool match;
+
   display_help();
 
-  char str1[256], str2[256];
+  str1[256] = get_archive_string();
+  str2[256] = get_query_string();
+  str3[256] = get_replacement_string();
 
-  puts("\n Enter the search string: ");
-//  sscanf(%s, str1);
-// printf("The original string is: %s", str1);
+  // search_string_for_query_string()
+  // replace_search_string_with_query_string()
+  // display_result_string()
 
-  puts("\n Enter the query string: ");
-  //gets(str2);
-  //printf("The query string is: %s", str2);
-  //
-  puts("\n Enter the newer string: ");
+//  for( ptr1=str1; ptr1; ptr1++ )
+ //   printf("char is %s", ptr1 );
 
-  puts("\n The result string is: FINAL ANSWER HERE ");
+  printf("\n Archive String: %s", str1);
+  printf("\n Query String: %s", str2);
+  printf("\n Replacement String: %s", str3);
+  printf("\n Result String: COMING SOON \n");
 
-}
+//  match = false;
+  ptr2 = str2;
+/*
+  for( ptr1=str1; ptr1; ptr1++ )
+  {
+    printf("\n comparing %c with %c", *ptr1, *ptr2);
+    if( *ptr1 == *ptr2 )
+    {
+      ptr2++;
+      if( *ptr2 == "\0" )
+      {
+//	match = true;
+	printf("\n match!");
+      }
+    }
+    else
+    {
+      ptr2 = str2;
+    }
+  } // end for() loop through str1
+*/
 
-
+} // end main
