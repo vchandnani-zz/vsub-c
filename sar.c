2daab0b4f487829f5ef205b3ab4abbf984dcad5e
Vishal Chandnani <vschandnani@gmail.com>
2013-12-05 23:14:11 -0500

    [VC] "search and replace" strings program
    - added function to get archive, query and replacement strings

--- sar.c
+++ sar.c
@@ -6,32 +6,22 @@ main()
   // TO DO: string length limits
   char str1[128], str2[32], str3[32], str4[1024];
   char *ptr1, *ptr2, *ptr3, *ptr4, *ptr5;
-  char *match_ptr1;
+  char *match_ptr1, *copy_ctr1, *copy_ptr2;
   int ctr1, ctr2;
   int match_ctr1;
 
   ptr1 = ptr2 = ptr3 = ptr4 = ptr5 = NULL;
-  match_ptr1 = NULL;
+  match_ptr1 = copy_ctr1 = copy_ptr2 = NULL;
   ctr1 = ctr2 = 0;
   match_ctr1 = 0;
 
   display_help();
-
-  printf("  Enter the archive string: ");
-  scanf("%s", str1);
-  fflush(stdin);
-
-  printf("  Enter the query string: ");
-  scanf("%s", str2);
-  fflush(stdin);
-
-  printf("  Enter the replacement string: ");
-  scanf("%s", str3);
-  fflush(stdin);
+  get_input_strings(str1, str2, str3);
 
   ctr2 = 0;
   match_ctr1 = 0;
   ptr4 = str4;
+  copy_ctr1 = str1;
 
   for( ptr1=str1, ptr2=str2, ctr1=0; *ptr1!='\0'; ptr1++, ctr1++ )
   {
@@ -43,14 +33,15 @@ main()
       ctr2++;
       if( *ptr2 == '\0' )
       {
+	copy_ptr2 = match_ptr1;
 	match_ptr1 = match_ptr1 - strlen(str2) + 1;
 	printf("\n  MATCH: archive string contains query string at array index %d, character %c", ctr1-1, *match_ptr1);
 
 	// copy characters from beginning of archive string str1 until match location
-	for( ptr5=str1; ptr5<match_ptr1; ptr4++, ptr5++ ) 
+	for( ptr5=copy_ctr1; ptr5<match_ptr1; ptr4++, ptr5++ ) 
 	{
 	  *ptr4 = *ptr5;
-	  printf("\n  STR1: COPY %c to %c", *ptr5, *ptr4);
+          printf("\n  STR1: COPY %c to %c", *ptr5, *ptr4);
 	}
 	// copy characters from replacement string str3 (replacing query string str2)
 	for( ptr5=str3; *ptr5!='\0'; ptr4++, ptr5++ )
@@ -59,6 +50,7 @@ main()
 	  printf("\n  STR3: COPY %c to %c", *ptr5, *ptr4);
 	}
 
+        copy_ctr1 = ptr1;
 	ptr2 = str2;
 	match_ctr1++;
 	ctr2 = 0;
@@ -68,6 +60,18 @@ main()
     {
       ptr2 = str2;
       ctr2 = 0;
+
+        // copy characters from last archive string match until end of archive string
+        //for( ; *copy_ptr2!='\0'; copy_ptr2++)
+	/*
+        while( *copy_ptr2!='\0' )
+        {
+          *ptr4 = *copy_ptr2;
+      	  printf("\n  STR4: COPY %c to %c", *copy_ptr2, *ptr4);
+      	  ptr4++;
+	  copy_ptr2++;
+        }
+	*/
     }
   } 
 
@@ -99,4 +103,20 @@ display_help()
   puts("  Query String:       \"world\"");
   puts("  Replacement String: \"people\"");
   puts("  Result String:      \"hello people\"");
+}
+
+get_input_strings(str1, str2, str3)
+char str1[128], str2[32], str3[32];
+{
+  printf("  Enter the archive string: ");
+  scanf("%s", str1);
+  fflush(stdin);
+
+  printf("  Enter the query string: ");
+  scanf("%s", str2);
+  fflush(stdin);
+
+  printf("  Enter the replacement string: ");
+  scanf("%s", str3);
+  fflush(stdin);
 }
