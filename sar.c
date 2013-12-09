08d493c58441ca076889d4f76e3f0d780e7437ab
Vishal Chandnani <vschandnani@gmail.com>
2013-12-08 17:30:34 -0800

    [VC] "search and replace" strings program
    - renamed pointer variables used to detect string matches
    - copy remaining characters from archive to result string
    (after last match location)

--- sar.c
+++ sar.c
@@ -6,22 +6,20 @@ main()
   // TO DO: string length limits and validations
   char str1[128], str2[32], str3[32], str4[1024];
   char *ptr1, *ptr2, *ptr3, *ptr4, *ptr5;
-  char *match_ptr1, *copy_ctr1, *copy_ptr2;
+  char *start_ptr1, *match_ptr1, *copy_ctr1, *copy_ptr2;
   int ctr1, ctr2;
   int match_ctr1;
 
   ptr1 = ptr2 = ptr3 = ptr4 = ptr5 = NULL;
-  match_ptr1 = copy_ctr1 = copy_ptr2 = NULL;
+  start_ptr1 = match_ptr1 = copy_ctr1 = copy_ptr2 = NULL;
   ctr1 = ctr2 = 0;
   match_ctr1 = 0;
 
   display_help();
   get_strings(str1, str2, str3);
 
-  ctr2 = 0;
-  match_ctr1 = 0;
   ptr4 = str4;
-  copy_ctr1 = str1;
+  start_ptr1 = str1;
 
   for( ptr1=str1, ptr2=str2, ctr1=0; *ptr1!='\0'; ptr1++, ctr1++ )
   {
@@ -38,11 +36,13 @@ main()
 	printf("\n  MATCH: archive string contains query string at array index %d, character %c", ctr1-1, *match_ptr1);
 
 	// copy characters from beginning of archive string str1 until match location
-	for( ptr5=copy_ctr1; ptr5<match_ptr1; ptr4++, ptr5++ ) 
+	for( ptr5=start_ptr1; ptr5<match_ptr1; ptr4++, ptr5++ ) 
 	{
 	  *ptr4 = *ptr5;
           printf("\n  STR1: COPY %c to %c", *ptr5, *ptr4);
 	}
+        start_ptr1 = match_ptr1 + strlen(str2);
+
 	// copy characters from replacement string str3 (replacing query string str2)
 	for( ptr5=str3; *ptr5!='\0'; ptr4++, ptr5++ )
 	{
@@ -50,7 +50,6 @@ main()
 	  printf("\n  STR3: COPY %c to %c", *ptr5, *ptr4);
 	}
 
-        copy_ctr1 = ptr1;
 	ptr2 = str2;
 	match_ctr1++;
 	ctr2 = 0;
@@ -60,22 +59,17 @@ main()
     {
       ptr2 = str2;
       ctr2 = 0;
-
-        // copy characters from last archive string match until end of archive string
-        //for( ; *copy_ptr2!='\0'; copy_ptr2++)
-	/*
-        while( *copy_ptr2!='\0' )
-        {
-          *ptr4 = *copy_ptr2;
-      	  printf("\n  STR4: COPY %c to %c", *copy_ptr2, *ptr4);
-      	  ptr4++;
-	  copy_ptr2++;
-        }
-	*/
     }
   } 
 
-  // terminate final result string pointer correctly
+  // copy remaining characters from archive to result string (after last match)
+  for( ptr5=start_ptr1; *ptr5!='\0'; ptr4++, ptr5++ )
+  {
+    *ptr4 = *ptr5;
+    printf("\n  AFTER LAST MATCH STR1: COPY %c to %c", *ptr5, *ptr4);
+  }
+
+  // terminate final result string
   *ptr4 = '\0';
 
   if( match_ctr1 == 0)
@@ -125,4 +119,5 @@ char str1[128], str2[32], str3[32], str4[1024];
   printf("\n  Query String: %s", str2);
   printf("\n  Replacement String: %s", str3);
   printf("\n  Final Result String: %s", str4);
+  printf("\n");
 }
