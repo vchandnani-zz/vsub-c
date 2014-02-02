50c9f202120e9c9731f09575f21f5d0fe078bfd4
Vishal Chandnani <vschandnani@gmail.com>
2014-02-01 21:38:54 -0500

    [VC] "search and replace" strings program
    - tested basic sunny-day scenarios
    - modified display_help function for basic sunny-day scenarios

--- sar.c
+++ sar.c
@@ -31,26 +31,26 @@ main()
 		if( match_ptr )
 		{ 
 			match_ctr ++;
-			printf("\n if - match ctr = %d", match_ctr);
-			printf( "\n MATCH %d: need to start copy/replace", match_ctr );
+// printf("\n if - match ctr = %d", match_ctr);
+// printf( "\n MATCH %d: need to start copy/replace", match_ctr );
 			// RESULT STRING: copy from archive string start location to match location
 			copy_strings( start_ptr, match_ptr, str4, copy_ptr );
 			copy_ptr += match_ptr - start_ptr;
 			// RESULT STRING: copy entire replacement string, i.e. replace query string
 			copy_strings( ptr3, ptr3 + strlen( str3 ), str4, copy_ptr );
-      printf( "\n MATCH: RESULT STR4: %s", str4 );
+// printf( "\n MATCH: RESULT STR4: %s", str4 );
 			start_ptr = match_ptr + strlen(str2);
 			copy_ptr += strlen( str3 );
-			printf( "\n continue searching from %c", *start_ptr );
+// printf( "\n continue searching from %c", *start_ptr );
 		}
 		else
 		{
-			printf("\n else - match ctr = %d", match_ctr);
+// printf("\n else - match ctr = %d", match_ctr);
 			if( match_ctr != 0)
 			{
 				// RESULT STRING: copy from archive string last match location until end
 				copy_strings( start_ptr, str1 + strlen( str1 ), str4, copy_ptr );
-        printf( "\n NO MATCH: RESULT STR4: %s", str4 );
+// printf( "\n NO MATCH: RESULT STR4: %s", str4 );
 			}
 			else
 			{
@@ -65,46 +65,47 @@ main()
 
 display_help()
 {
-  system("tput clear");
-  puts("  Welcome to the String Search and Replace Program.");
-  puts("  Inputs:  Archive String, Query String and Replacement String.");
-  puts("  Outputs: Result String.");
-  puts("  The Search and Replace Program searches the archive string for a query string.");
-  puts("  If the archive string contains the entire query string, it replaces the query");
-  puts("  string with the replacement string and displays the final result string.");
-  puts("  Example:");
-  puts("  Archive String:     \"hello world\"");
-  puts("  Query String:       \"world\"");
-  puts("  Replacement String: \"people\"");
-  puts("  Result String:      \"hello people\"");
-  puts("");
+  system( "tput clear" );
+  puts( "  Welcome to the String Search and Replace Program."  );
+  puts( "  The Search and Replace Program searches the archive string for a query string." );
+  puts( "  If the archive string contains the entire query string, it replaces the query" );
+  puts( "  string with the replacement string and displays the final result string." );
+  puts( "  Inputs:  Archive String, Query String and Replacement String." );
+  puts( "  Outputs: Result String" );
+  puts( "  Example:" );
+  puts( "  --------" );
+  puts( "  Archive String:     \"abcd\"" );
+  puts( "  Query String:       \"bc\"" );
+  puts( "  Replacement String: \"1234\"" );
+  puts( "  Result String:      \"a1234d\"" );
+  puts( "" );
 }
 
-get_strings(str1, str2, str3)
+get_strings( str1, str2, str3 )
 char str1[32], str2[4], str3[8];
 {
-	get_string(str1, "archive", 32);
-	get_string(str2, "query", 4);
-	get_string(str3, "replacement", 8);
+	get_string( str1, "archive", 32 );
+	get_string( str2, "query", 4 );
+	get_string( str3, "replacement", 8 );
 }
 
-get_string(str, name, max_len)
+get_string( str, name, max_len )
 char str[32], name[16];
 int max_len;
 {
 	for(;;)
 	{
-		printf("  Enter the %s string (Maximum length = %d characters): ", name, max_len);
-		scanf("%s", str);
-		fflush(stdin);
-		if( strlen(str) <= max_len )
+		printf( "  Enter the %s string (Maximum length = %d characters): ", name, max_len );
+		scanf( "%s", str );
+		fflush( stdin );
+		if( strlen( str ) <= max_len )
 		{
 			break;
 		}
 	}
 }
 
-char *search_strings(str1, str2, start_ptr)
+char *search_strings( str1, str2, start_ptr )
 char str1[32], str2[4];
 char *start_ptr;
 {
@@ -116,7 +117,7 @@ char *start_ptr;
 
   for( ptr1=start_ptr, ptr2=str2, ctr1=0; *ptr1!='\0'; ptr1++, ctr1++ )
   {
-    printf("\n array index %d - comparing STR1: %c with STR2: %c", ctr1, *ptr1, *ptr2);
+// printf("\n array index %d - comparing STR1: %c with STR2: %c", ctr1, *ptr1, *ptr2);
     if( *ptr1 == *ptr2 )
     {
       match_ptr1 = ptr1;
@@ -126,8 +127,8 @@ char *start_ptr;
       if( *ptr2 == '\0' )
       {
 				// calculate match start location 
-				match_ptr1 = match_ptr1 - strlen(str2) + 1;
-			  printf( "\n MATCH: STR1 contains STR2 at character %c", *match_ptr1 );
+				match_ptr1 = match_ptr1 - strlen( str2 ) + 1;
+// printf( "\n MATCH: STR1 contains STR2 at character %c", *match_ptr1 );
 				ptr2 = str2;
 				ctr2 = 0;
 				return match_ptr1;
@@ -135,7 +136,7 @@ char *start_ptr;
 		}
     else
     {
-		  printf( "\n NO MATCH ");
+// printf( "\n NO MATCH ");
       ptr2 = str2;
       ctr2 = 0;
     }
@@ -143,7 +144,7 @@ char *start_ptr;
 	return match_ptr1;
 }
 
-char *copy_strings( start_ptr, end_ptr, dest_str, dest_copy_ptr)
+char *copy_strings( start_ptr, end_ptr, dest_str, dest_copy_ptr )
 char dest_str[1024];
 char *start_ptr, *end_ptr, *dest_copy_ptr;
 {
@@ -152,16 +153,16 @@ char *start_ptr, *end_ptr, *dest_copy_ptr;
 	for( ptr1 = start_ptr, ptr2 = dest_copy_ptr; ptr1 < end_ptr; ptr1 ++, ptr2 ++ )
 	{
 		*ptr2 = *ptr1;
-	  printf( "\n DEST STR: copied %c to %c", *ptr1, *ptr2 ); 
+// printf( "\n DEST STR: copied %c to %c", *ptr1, *ptr2 ); 
 	}
 }
 
-display_results(str1, str2, str3, str4)
+display_results( str1, str2, str3, str4 )
 char str1[32], str2[4], str3[8], str4[1024];
 {
-  printf("\n  Archive String: %s", str1);
-  printf("\n  Query String: %s", str2);
-  printf("\n  Replacement String: %s", str3);
-  printf("\n  Final Result String: %s", str4);
-  printf("\n");
+  printf( "\n  Archive String: %s", str1 );
+  printf( "\n  Query String: %s", str2 );
+  printf( "\n  Replacement String: %s", str3 );
+  printf( "\n  Final Result String: %s", str4 );
+  printf( "\n" );
 }
