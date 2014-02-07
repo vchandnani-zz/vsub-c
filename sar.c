21bbc0088a4ccfcbbe91b412f865ee78f24919cf
Vishal Chandnani <vschandnani@gmail.com>
2014-02-06 22:40:27 -0500

    [VC] "search and replace" strings program
    - added mechanism to enable/disable debugging code/printf statements
    - bug fix: archive string pointer reaches end-of-string before query string pointer

--- sar.c
+++ sar.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+// debug_ctr = 1 => debugging code is enabled
+// debug_ctr = 0 => debugging code is disabled
+int debug_ctr = 0;
+
 main()
 {
   char *search_strings();
@@ -28,37 +32,43 @@ main()
 //	while( *start_ptr != '\0' )
 	while( start_ptr <= ( str1 + strlen( str1 ) ) ) 
 	{
+if( debug_ctr == 1 )
 printf( "\n START ptr = %c", *start_ptr );
   	match_ptr = search_strings( str1, str2, start_ptr );
 		if( match_ptr )
 		{ 
 			match_ctr ++;
+if( debug_ctr == 1 )
 printf( "\n if - MATCH ctr %d: need to start copy/replace", match_ctr );
 			// RESULT STRING: copy from archive string start location to match location
 			copy_strings( start_ptr, match_ptr, str4, copy_ptr );
 			copy_ptr += match_ptr - start_ptr;
 			// RESULT STRING: copy entire replacement string, i.e. replace query string
 			copy_strings( ptr3, ptr3 + strlen( str3 ), str4, copy_ptr );
+if( debug_ctr == 1 )
 printf( "\n MATCH: RESULT STR4: %s", str4 );
 			start_ptr = match_ptr + strlen(str2);
 			copy_ptr += strlen( str3 );
+if( debug_ctr == 1 )
 printf( "\n continue searching from %c", *start_ptr );
 		}
 		else
 		{
+if( debug_ctr == 1 )
 printf("\n else - match ctr = %d", match_ctr);
 			if( match_ctr != 0)
 			{
 				// RESULT STRING: copy from archive string last match location until end
+if( debug_ctr == 1 )
 printf( "\n NO MATCH: copy from last match start ptr %c until end", *start_ptr );
 				copy_strings( start_ptr, str1 + strlen( str1 ), str4, copy_ptr );
+if( debug_ctr == 1 )
 printf( "\n NO MATCH: RESULT STR4: %s", str4 );
 				break;
 			}
 			else
 			{
-				printf( "\n The archive string does not contain the query string." );
-				strcpy( str4, "N/A" );
+				strcpy( str4, "The archive string does not contain the query string." );
 				break;
 			}
 			start_ptr ++;
@@ -122,17 +132,19 @@ char *start_ptr;
 
   for( ptr1=start_ptr, ptr2=str2, ctr1=0; *ptr1!='\0'; ptr1++, ctr1++ )
   {
+if( debug_ctr == 1 )
 printf( "\n array index %d - comparing STR1: %c with STR2: %c", ctr1, *ptr1, *ptr2);
     if( *ptr1 == *ptr2 )
     {
-      match_ptr1 = ptr1;
+      // match_ptr1 = ptr1; // VC TEST
       match_ctr1++;
 			ptr2++;
       ctr2++;
       if( *ptr2 == '\0' )
       {
 				// calculate match start location 
-				match_ptr1 = match_ptr1 - strlen( str2 ) + 1;
+				match_ptr1 = ptr1 - strlen ( str2 ) + 1; // match_ptr1 - strlen( str2 ) + 1;
+if( debug_ctr == 1 )
 printf( "\n MATCH: STR1 contains STR2 at character %c", *match_ptr1 );
 				ptr2 = str2;
 				ctr2 = 0;
@@ -141,6 +153,7 @@ printf( "\n MATCH: STR1 contains STR2 at character %c", *match_ptr1 );
 		}
     else
     {
+if( debug_ctr == 1 )
 printf( "\n search_strings: NO MATCH ");
       match_ptr1 = NULL;
       ptr2 = str2;
@@ -160,6 +173,7 @@ char *start_ptr, *end_ptr, *dest_copy_ptr;
 	for( ptr1 = start_ptr, ptr2 = dest_copy_ptr; ptr1 < end_ptr; ptr1 ++, ptr2 ++ )
 	{
 		*ptr2 = *ptr1;
+if( debug_ctr == 1 )
 printf( "\n DEST STR: copied %c to %c", *ptr1, *ptr2 ); 
 	}
 }
